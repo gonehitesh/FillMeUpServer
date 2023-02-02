04f7c9a19f7c336ecffdc337bfedd2a504f961dd
gonehitesh <gonehitesh@gmail.com>
2023-02-01 21:04:53 -0500

    updated env file

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/users");
-const db = require("../config/key").MongodbURI;
+const db = process.env.MongodbURI;
 
 router.get("/", (req, res) => {
   res.send({ message: "hey im object" });
